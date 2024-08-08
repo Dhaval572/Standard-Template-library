0e3146ad58fe139dbb10fe954d8fbacb8144b217
Dhaval572 <dp9718776@gmail.com>
2024-08-08 20:16:09 +0530

    Creating list

---
+++ list.cpp
@@ -0,0 +1,44 @@
+// Creating linked list in c++ using standerd template library
+#include <iostream>
+#include <list>
+using namespace std;
+
+void display(list<int> &lst)// for Display list
+{
+	list<int>::iterator it;// Creating iterater named it
+	for (it = lst.begin(); it != lst.end(); it++)
+	{
+		cout << *it << " ";
+	}
+}
+
+int main()
+{	
+	list<int> list1; // Declaration of list
+	int element,size;
+	cout << "Enter the size of your list: ";
+	cin >> size;
+	for (int i = 0; i < size; i++)
+	{
+		cout << "Enter (" << i + 1 << ") element: ";
+		cin >> element;
+		list1.push_back(element); // adding element in list
+	}
+	cout << "You entered: ";
+	display(list1);
+	return 0;
+}
+
+/*note:
+
+Syntex to declare list using stl:
+list<data_type> list_name;
+
+-> if you want to create linked list using stl then you should have to include 'list' library (#inclue <list>)
+-> iterator is special object which points to the element of linked list,vector etc;
+-> begin() points first element of list
+-> end() points last element of list
+
+for more information visit this website:
+cplusplus.com
+*/

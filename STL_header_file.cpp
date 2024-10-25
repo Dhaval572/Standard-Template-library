57d1885ae552e4815539cdac132ddf9e999e3b61
Dhaval572 <dp9718776@gmail.com>
2024-10-25 19:49:33 +0530

    Create STL_header_file.cpp

---
+++ STL_header_file.cpp
@@ -0,0 +1,27 @@
+// Examples of STL header files
+
+#include <array>
+#include <vector>
+#include <deque>
+#include <list>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <stack>
+#include <queue>
+#include <algorithm>
+#include <iterator>
+#include <functional>
+#include <utility>
+#include <memory>
+#include <string>
+#include <numeric>
+#include <complex>
+#include <valarray>
+#include <chrono>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+
+// This is most commonly used header file in Standard template library

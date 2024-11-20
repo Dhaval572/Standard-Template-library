b6292a3ebf61a311163ed4fa4b5104631e77cbd1
Dhaval572 <dp9718776@gmail.com>
2024-11-20 19:27:57 +0530

    Minor change

--- Problem 1 find_max_and_min_in_vector.cpp
+++ Problem 1 find_max_and_min_in_vector.cpp
@@ -6,14 +6,12 @@ using namespace std;
 void findMax(vector<int> &vec)
 {
 	int max = vec.at(0); // Assume that first element is maximum
-	for (int i = 0; i < vec.size(); i++)
+	for (int i = 1; i < vec.size(); i++)
 	{
-		if (max < vec.at(i)) // If you find maximum than max
+		// If you find maximum than max
+		if (max < vec.at(i)) 
 		{
-			// Swap max and at(i)
-			int temp = max;
-			max = vec.at(i);
-			vec.at(i) = temp;
+			max = vec.at(i); // Update the max
 		}
 	}
 
@@ -23,14 +21,12 @@ void findMax(vector<int> &vec)
 void findMin(vector<int> &vec)
 {
 	int min = vec.at(0); // Assume that first is minimum
-	for(int i = 0; i < vec.size(); i++) // If you find minimum than min
+	for(int i = 1; i < vec.size(); i++) 
 	{
+		// If you find minimum than min
 		if(min > vec.at(i))
 		{
-			// Swap max and at(i)
-			int temp = min;
 			min = vec.at(i);
-			vec.at(i) = temp;
 		}
 	}
 

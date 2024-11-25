2edb746b001837080cb50bf6452a550cbf1fcd13
Dhaval572 <dp9718776@gmail.com>
2024-11-25 18:20:47 +0530

    Create Problem_explaination_(1).cpp

---
+++ Problem_explaination_(1).cpp
@@ -0,0 +1,46 @@
+/*
+Explaination of problem:
+
+	Array Rotation refers to the process of shifting the elements of an array by a certain number of positions either to the left or to the right.
+
+	There are two type of rotation 1). left rotation and 2). right rotation.
+
+	Example of right rotation:
+	
+		Input array: [1, 2, 3, 4, 5]
+		Rotate by k = 2 positions:
+
+		1). After the first right rotation: [5, 1, 2, 3, 4]
+		2). After the second right rotation: [4, 5, 1, 2, 3]
+		
+		The final array after 2 right rotations is [4, 5, 1, 2, 3].
+
+	Example of left rotation:
+
+		Input array: [1, 2, 3, 4, 5]
+
+		1). After the first left rotation: [2, 3, 4, 5, 1]
+		2). After the second left rotation: [3, 4, 5, 1, 2]
+		
+		After the second left rotation: [3, 4, 5, 1, 2]
+
+	Practical Applications of Array Rotation:
+
+	1). Circular Queues:
+
+		In many data structures like circular buffers or queues, elements can be 
+		rotated to simulate continuous data flow.
+
+	2). Image Processing:
+
+		Rotating an image (represented as a matrix or an array) by a certain angle 
+		can be thought of as rotating the pixels.
+
+	3).	Game Development:
+
+		Arrays may be rotated to represent movements, such as rotating a character’s direction or rotating objects in a 2D game world.
+
+	So, this was only problem explaination of Rotation.
+
+	I hope you understand everything!	
+*/

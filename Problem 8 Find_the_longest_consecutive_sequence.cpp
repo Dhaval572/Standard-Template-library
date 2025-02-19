cc97f92d361217932702cb828c74dd76e869cb36
Dhaval <dp9718776@gmail.com>
2025-02-19 23:39:20 +0530

    Update Problem 8 Find_the_longest_consecutive_sequence.cpp

--- Problem 8 Find_the_longest_consecutive_sequence.cpp
+++ Problem 8 Find_the_longest_consecutive_sequence.cpp
@@ -6,32 +6,21 @@ using namespace std;
 
 int longestConsecutive(vector<int> &nums)
 {
-    if (nums.empty())
-        return 0;
-
-    // Store elements in a hash set
     unordered_set<int> numSet(nums.begin(), nums.end());
-
     int longest = 0;
 
-    for (int num : numSet)
+    for (int n : numSet)
     {
-        if (numSet.find(num - 1) == numSet.end())
+        if (numSet.find(n - 1) == numSet.end())
         {
-            int currentNum = num;
-            int currentStreak = 1;
-
-            while (numSet.find(currentNum + 1) != numSet.end())
+            int length = 1;
+            while (numSet.find(n + length) != numSet.end())
             {
-                // Count the sequence length
-                currentNum++;
-                currentStreak++;
+                length++;
             }
-
-            longest = max(longest, currentStreak); // Update longest sequence length
+            longest = max(longest, length);
         }
     }
-
     return longest;
 }
 
@@ -44,15 +33,16 @@ int main()
 
 /*
 Dry Run Table:
-+------+--------+---------------+--------------------+----------------+------------------+
-| Step | Number | num-1 Exists? | Start of Sequence? | Sequence Found | Longest Updated? |
-|----------------------------------------------------------------------------------------|
-| 1    | 100    | No            | Yes                | [100]          | No               |
-| 2    | 4      | No            | Yes                | [4]            | No               |
-| 3    | 200    | No            | Yes                | [200]          | No               |
-| 4    | 1      | No            | Yes                | [1, 2, 3, 4]   | Yes (longest = 4)|
-| 5    | 3      | Yes (2)       | No                 | -              | No               |
-| 6    | 2      | Yes (1)       | No                 | -              | No               |
-+------+--------+---------------+--------------------+----------------+------------------+
++------+--------+---------------+--------------------+------------------+------------------+
+| Step | Number | num-1 Exists? | Start of Sequence? | Sequence Length  | Longest Updated? |
++------+--------+---------------+--------------------+------------------+------------------+
+| 1    | 100    | No            | Yes                | 1                | No (longest=1)   |
+| 2    | 4      | No            | Yes                | 1                | No (longest=1)   |
+| 3    | 200    | No            | Yes                | 1                | No (longest=1)   |
+| 4    | 1      | No            | Yes                | 4 (1,2,3,4)      | Yes (longest=4)  |
+| 5    | 3      | Yes (2)       | No                 | -                | No               |
+| 6    | 2      | Yes (1)       | No                 | -                | No               |
++------+--------+---------------+--------------------+------------------+------------------+
+
 Final Output: 4
 */

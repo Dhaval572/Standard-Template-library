58443f45c1c9e2ed1d22101a4b9494bfde91f225
Dhaval <dp9718776@gmail.com>
2025-05-04 22:58:47 +0530

    Added advantages and use cases

--- Tuple_example.cpp
+++ Tuple_example.cpp
@@ -3,19 +3,54 @@
 #include <tuple>
 using namespace std;
 
+/*
+    +---------------------+
+    | Advantages of tuple |
+    +---------------------+
+
+    1. Supports heterogeneity: Can store different types together.
+
+    2. Standardized and type-safe: Provided by STL, with full type checking.
+
+    3. Decomposition support: Can be unpacked using std::tie() or structured bindings.
+
+    4. Extensible: Easily used in templated or generic code.
+
+    5. Comparison operators: Element-wise comparison is built-in.
+
+    6. Type safety over C-style structs
+
+    +-----------+
+    | Use cases |
+    +-----------+
+
+    1. Returning Multiple Values from Functions
+
+    2. Structured Bindings for Clean Unpacking
+
+    3. Replacing std::pair for Complex Cases
+
+    4. Compile-Time Data Processing
+
+        Example:
+
+            constexpr auto sizes = make_tuple(sizeof(int), sizeof(double), sizeof(char));
+            static_assert(get<0>(sizes) == 4); // Compile-time check
+
+*/
+
 // Print all elements using lambda function
 auto printTuple = [](const auto &tup)
 {
     apply([](const auto &...args)
-          {
-              ((cout << args << '\n'), ...); 
-          },
+          { ((cout << args << '\n'), ...); },
           tup);
-    cout << '\n'; 
+    cout << '\n';
 };
 
 int main()
 {
+
     tuple<int, char, float> myTup(12, 'c', 234.34f);
 
     printTuple(myTup);
